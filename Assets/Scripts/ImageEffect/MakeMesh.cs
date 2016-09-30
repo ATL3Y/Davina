f3ff8b97777b908c575448c0fb22670d2e819ce8
Atley <atleyl@gmail.com>
2016-09-29 18:18:32 -0700

    update

--- Assets/Scripts/ImageEffect/MakeMesh.cs
+++ Assets/Scripts/ImageEffect/MakeMesh.cs
@@ -6,33 +6,44 @@ public class MakeMesh : MonoBehaviour
 	private Color _color;
 	bool once = true;
 	Mesh m_mesh;
+	Mesh m_meshOriginal;
 
 	// Use this for initialization
 	public void Start () 
 	{
-		m_mesh = new Mesh ();
 
 		_color = new Color ();
 		ColorUtility.TryParseHtmlString ("#FFBDF7FF", out _color);
 		if (GetComponent<MeshFilter> () != null) m_mesh = GetComponent<MeshFilter> ().sharedMesh;
 		if ( GetComponent< MeshFilter >() != null ) m_mesh = GetComponent< MeshFilter >().mesh ;
 		else if ( GetComponent< SkinnedMeshRenderer >() != null ) m_mesh = GetComponent< SkinnedMeshRenderer >().sharedMesh;
+
+		/*
+		m_meshOriginal = new Mesh ();
+		m_meshOriginal.SetVertices (m_mesh.vertices);
+		m_meshOriginal.SetTriangles (m_mesh.triangles);
+		m_meshOriginal.SetUVs (m_mesh.GetUVs ());
+		*/
 	}
 
 	public void Update ()
 	{
+		/*
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (m_mesh.GetTopology (0) != MeshTopology.Lines)
+				m_mesh.SetIndices (m_mesh.triangles, MeshTopology.Lines, 0);
+		} else if (Input.GetKeyUp (KeyCode.Space)) {
+			m_mesh.Se
+		}
+		*/
 		
 	}
 	
-	// Update is called once per frame
+
 	public void LateUpdate (  ) 
 	{
-		//if (once) {
 		if ( m_mesh.GetTopology(0) != MeshTopology.Lines )
 			m_mesh.SetIndices (m_mesh.triangles, MeshTopology.Lines, 0);
-			once = false;
-		//}
-
-		//AxKDebugLines.AddMesh (transform, GetComponent<MeshFilter>().mesh, _color, 0);
 	}
+
 }
