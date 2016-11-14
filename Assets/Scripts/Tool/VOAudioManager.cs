ca31ae34b9d81e6ca287e1798ee3a0da9d4a3485
russellhonor <arcanekids@gmail.com>
2016-11-14 00:22:38 -0800

    forgotten changes

---
+++ Assets/Scripts/Tool/VOAudioManager.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TriggerAudioFX
+    : MonoBehaviour {
+
+	[SerializeField] List<AudioClip> enterClips;
+	[SerializeField] List<AudioClip> exitClips;
+
+	private bool onceEnter = false;
+	private bool onceExit = false;
+	protected int i=0;
+	protected int j=0;
+
+	protected AudioSource sourceEnter;
+	protected AudioSource sourceExit;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		if (enterClips != null) {
+			sourceEnter = gameObject.AddComponent<AudioSource> ();
+			sourceEnter.playOnAwake = sourceEnter.loop = false;
+			sourceEnter.volume = 1f;
+			sourceEnter.spatialBlend = 1f;
+		}
+
+		if (exitClips != null) {
+			sourceExit = gameObject.AddComponent<AudioSource> ();
+			sourceExit.playOnAwake = sourceExit.loop = false;
+			sourceExit.volume = 1f;
+			sourceExit.spatialBlend = 1f;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+	/*
+	void OnTriggerEnter(Collider other)
+	{
+		if (!onceEnter && other.gameObject.tag == "Player" && enterClips.Count > 0) { //col.gameObject.tag == "Player"
+
+			StartCoroutine(PlayNextEnter ());
+			onceEnter = true;
+		} 
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if (!onceExit && other.gameObject.tag == "Player" && exitClips.Count > 0) { //col.gameObject.tag == "Player"
+			
+			StartCoroutine(PlayNextExit ());
+			onceExit = true;
+		} 
+	}
+	*/
+	protected void OnEnable(){
+
+		M_Event.logicEvents [(int)LogicEvents.Characters] += OnCharacters;
+		M_Event.logicEvents [(int)LogicEvents.End] += OnEnd;
+	}
+
+	protected void OnDisable(){
+
+		M_Event.logicEvents [(int)LogicEvents.Characters] -= OnCharacters;
+		M_Event.logicEvents [(int)LogicEvents.End] -= OnEnd;
+	}
+
+	void OnCharacters( LogicArg arg ){
+		if (!onceEnter && enterClips.Count > 0) { //col.gameObject.tag == "Player"
+
+			StartCoroutine(PlayNextEnter ());
+			onceEnter = true;
+		} 
+	}
+
+	void OnEnd( LogicArg arg ){
+		if (!onceEnter && enterClips.Count > 0) { //col.gameObject.tag == "Player"
+
+			StartCoroutine(PlayNextEnter ());
+			onceEnter = true;
+		} 
+	}
+
+	IEnumerator PlayNextEnter(){
+			
+		sourceEnter.clip = enterClips [i];
+		sourceEnter.Play ();
+		//print ("playing clips " + sourceEnter.clip.name);
+		yield return new WaitForSeconds (sourceEnter.clip.length);
+		i++;
+		if (i < enterClips.Count) {
+			StartCoroutine (PlayNextEnter ());
+		} else if (gameObject.tag == "End") {
+			LogicArg logicArg = new LogicArg (this);
+			M_Event.FireLogicEvent (LogicEvents.Credits, logicArg);
+		}
+	}
+
+	IEnumerator PlayNextExit(){
+
+		sourceExit.clip = exitClips [j];
+		sourceExit.Play ();
+		//print ("playing clips at " + j); // + sourceEnter.clip.name);
+		yield return new WaitForSeconds (sourceExit.clip.length);
+		j++;
+		if (j < exitClips.Count) {
+			StartCoroutine(PlayNextExit ());
+		}
+	}
+}
---
+++ Assets/Scripts/Tool/VOEventAudio.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VOEventAudio : MonoBehaviour {
+
+	[SerializeField] List<AudioClip> clips1;
+	[SerializeField] List<AudioClip> clips2;
+
+	protected int i=0;
+	protected int j=0;
+
+    protected AudioSource source;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if ( source == null) {
+            source = gameObject.AddComponent<AudioSource> ();
+            source.playOnAwake = source.loop = false;
+            source.volume = 1f;
+            source.spatialBlend = 1f;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	protected void OnEnable(){
+
+		M_Event.logicEvents [(int)LogicEvents.Characters] += OnCharacters;
+		M_Event.logicEvents [(int)LogicEvents.End] += OnEnd;
+	}
+
+	protected void OnDisable(){
+
+		M_Event.logicEvents [(int)LogicEvents.Characters] -= OnCharacters;
+		M_Event.logicEvents [(int)LogicEvents.End] -= OnEnd;
+	}
+
+	void OnCharacters( LogicArg arg ){
+		if ( clips1.Count > 0) { 
+			StartCoroutine(PlayNext ( clips1 ) );
+		} 
+	}
+
+	void OnEnd( LogicArg arg ){
+		if ( clips2.Count > 0) { 
+			StartCoroutine(PlayNext ( clips2 ) );
+		} 
+	}
+
+	IEnumerator PlayNext( List<AudioClip> clips )
+    {
+        yield return new WaitForSeconds( 3f ); //wait unitl match VO is over
+		source.clip = clips[i];
+		source.Play ();
+		yield return new WaitForSeconds (source.clip.length);
+		i++;
+        if ( i < clips.Count )
+        {
+            StartCoroutine( PlayNext( clips ) );
+        }
+        else
+        {
+            i = 0;
+            if ( gameObject.tag == "End" )
+            {
+                i = 0;
+                LogicArg logicArg = new LogicArg( this );
+                M_Event.FireLogicEvent( LogicEvents.Credits, logicArg );
+            }
+        }
+	}
+}
